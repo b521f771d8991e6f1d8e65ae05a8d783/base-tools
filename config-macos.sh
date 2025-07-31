6a81c0c3b9399337004f8a2614e3a191207116ae
Alexander M. Hipfl <emit-armoire-flap@duck.com>
2025-07-31 08:08:25 +0200

    updated config-macos

--- config-macos.sh
+++ config-macos.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/zsh
 
-brew install rust cargo
+xcode-select --install
+brew install rustup deno git cmake
+rustup default stable
+rustup target install wasm32-unknown-unknown
+cargo install bacon wasm-pack wasm-bindgen-cli
