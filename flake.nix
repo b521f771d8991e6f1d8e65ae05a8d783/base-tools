7f18a8c5f5e3108f7ffba7688215d422c461ceb5
Alexander M. Hipfl <emit-armoire-flap@duck.com>
2025-08-06 11:36:26 +0200

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -2,40 +2,80 @@
   description = "A Nix Flake with global packages for development environments";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/11cb3517b3af6af300dd6c055aeda73c9bf52c48";
-    flake-utils.url = "github:numtide/flake-utils/04c1b180862888302ddfb2e3ad9eaa63afc60cf8";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [
-      flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-linux
-      flake-utils.lib.system.x86_64-darwin flake-utils.lib.system.aarch64-darwin
-    ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = false;
-        };
-        lib = pkgs.lib;
-        stdenv = pkgs.stdenv;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem
+      [
+        flake-utils.lib.system.x86_64-linux
+        flake-utils.lib.system.aarch64-linux
+        flake-utils.lib.system.x86_64-darwin
+        flake-utils.lib.system.aarch64-darwin
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = false;
+          };
+          lib = pkgs.lib;
 
-        globalPackages = with pkgs; [
-          zsh git gnumake pkg-config cmake radicle-node rpm dpkg ninja jq # tools
-          clang clang-tools lld  # (Objective) C/++ toolchain
-          rustc cargo wasm-pack wasm-bindgen-cli bacon # rust toolchain
-          swift swiftpm # swift toolchain
-          nodejs # typescript toolchain
-        ] ++ lib.optionals stdenv.isLinux [ gcc gnustep-base gnustep-gui gnustep-make gnustep-libobjc ]
-          ++ lib.optionals stdenv.isDarwin [ libcxx apple-sdk ];
-    in {
-      devShells = {
-        default = pkgs.mkShell.override { }
+          stdenv = pkgs.stdenv;
+
+          global-packages =
+            with pkgs;
+            [
+              zsh
+              git
+              gnumake
+              pkg-config
+              cmake
+              radicle-node
+              rpm
+              ninja
+              jq # tools
+              clang
+              clang-tools
+              lld # (Objective) C/++ toolchain
+              rustc
+              cargo
+              wasm-pack
+              wasm-bindgen-cli
+              bacon # rust toolchain
+              swift
+              swiftpm # swift toolchain
+              nodejs # typescript toolchain
+            ]
+            ++ lib.optionals stdenv.isLinux [
+              gcc
+              gnustep-base
+              gnustep-gui
+              gnustep-make
+              gnustep-libobjc
+              dpkg
+            ]
+            ++ lib.optionals stdenv.isDarwin [
+              libcxx
+              apple-sdk
+            ];
+        in
         {
-            packages = with pkgs; globalPackages;
-        };
-      };
+          devShells = {
+            default = pkgs.mkShell.override { } {
+              packages = with pkgs; global-packages;
+            };
+          };
 
-      globalPackages = globalPackages;
-      }
-    );
+          global-packages = global-packages;
+          formatter = pkgs.nixfmt-tree;
+        }
+      );
 }
