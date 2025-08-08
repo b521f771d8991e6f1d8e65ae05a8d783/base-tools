d4499b35800c61de166fce6e9b7279fe3c069f46
b521f771d8991e6f1d8e65ae05a8d783 <177119872+b521f771d8991e6f1d8e65ae05a8d783@users.noreply.github.com>
2025-08-08 10:07:05 +0200

    Update config-macos.sh

--- config-macos.sh
+++ config-macos.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/zsh
 
 xcode-select --install
-brew install rustup deno git cmake
+brew install rustup deno git cmake llvm
 rustup default stable
 rustup target install wasm32-unknown-unknown
 cargo install bacon wasm-pack wasm-bindgen-cli
