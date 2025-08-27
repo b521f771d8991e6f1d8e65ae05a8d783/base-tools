4ec98dc59f0af9ccb1a058fecce31fac3dbedaae
b521f771d8991e6f1d8e65ae05a8d783 <177119872+b521f771d8991e6f1d8e65ae05a8d783@users.noreply.github.com>
2025-08-27 15:51:22 +0200

    Delete config-macos.sh

--- config-macos.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/zsh
-
-xcode-select --install
-brew update
-brew install rustup npm git cmake llvm
-rustup default stable
-rustup target install wasm32-unknown-unknown
-cargo install bacon wasm-pack wasm-bindgen-cli
